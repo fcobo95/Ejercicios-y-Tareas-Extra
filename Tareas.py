5165ff3344c4277ea87d8b5727039c8f6037df23
Erick Fernando Cobo Enriquez <ecoboe249@ulacit.ed.cr>
2017-03-30 01:37:25 -0600

    Termine el primer punto de la tarea. Pendiente punto 2 y 3.

--- Tareas.py
+++ Tareas.py
@@ -1,30 +1,75 @@
-class TaresServiciosWeb:
+# -------------------- CLASS FloatArray BEGINS ----------------------------------------------------------------------- #
+class FloatArray:
     # self means that the constructor is taking in the instance of that object.
-    def __init__(self, the_array):
-        self.the_array = the_array
+    # self is always taken as the first argument in the constructor parameters
+    def __init__(self):
+        self.the_array = []
         self.the_result_array = []
 
-    # This method will handle how to calculate the square root of 1000 numbers that go into an array.
-    def float_square_roots(self):
-        print('This prints the square root for each number in the array.')
+    def fill_array(self):
+        for each_number in range(20):
+            self.the_array.append(each_number)
 
+    def format(self, x, y):
+        return print('{} : {}'.format(x, y))
+
+    # This method will handle how to calculate the square root of 1000 numbers that go into an array.
+    def square_root(self):
+        print('This prints the square root for each number in the array.')
         for each_number in self.the_array:
             try:
                 square_root = each_number ** (1 / 2)
                 self.the_result_array.append(square_root)
+                self.format(each_number, square_root)
+            except Exception as ex:
+                return 'Something went wrong: ' + str(ex)
+        return ''
+
+    def subtract(self, number1, number2):
+        return number1 - number2
+
+    def subtract_n_from_array(self, n):
+        for each_number in self.the_array:
+            new_number = self.subtract(each_number, n)
+            self.format(each_number, new_number)
+        return 'Succesful operation'
+
+
+# -------------------- CLASS FloatArray ENDS ------------------------------------------------------------------------- #
+# Instance of FloatArray class
+floatClass = FloatArray()
+print('---------------BEGIN OF FLOATARRAY CLASS---------------' + '\n')
+FloatArray.fill_array(floatClass)
+FloatArray.square_root(floatClass)
+FloatArray.subtract_n_from_array(floatClass, 10)
+print('---------------END OF FLOATARRAY CLASS---------------' + '\n')
+
+
+# -------------------- CLASS IntegerArray BEGINS --------------------------------------------------------------------- #
+class IntegerArray:
+    def __init__(self):
+        self.the_array = []
+        self.the_result_array = []
+
+    def integer_square_roots(self):
+        import random
+        for each_number in range(1000):
+            try:
+                self.the_array.append(random.randint(1, 1000))
+            except Exception as ex:
+                return 'Something went wrong: ' + str(ex)
+        for each_number in self.the_array:
+            try:
+                square_root = each_number ** (1 / 2)
+                self.the_result_array.append(each_number + square_root)
                 print('{} : {}'.format(each_number, square_root))
             except Exception as ex:
                 return 'Something went wrong: ' + str(ex)
-        return 'Process finished with exit code 0'
+        return ''
 
-    def integer_square_roots(self):
-        the_result_array = []
-        the_array = []
-        import random
-        for each_number in range(random.randrange(0, 1000)):
-            print('')
-        return the_result_array
-
-
-array = TaresServiciosWeb([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16])
-print(TaresServiciosWeb.float_square_roots(array))
+# -------------------- CLASS IntegerArray ENDS ----------------------------------------------------------------------- #
+# Instance of IntegerArray class
+# intClass = IntegerArray()
+# print('---------------BEGIN OF INTEGERARRAY CLASS---------------' + '\n')
+# print(IntegerArray.integer_square_roots(intClass))
+# print('---------------END OF INTEGERARRAY CLASS---------------' + '\n')
