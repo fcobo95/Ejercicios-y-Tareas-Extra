bba6bf110b06cfc4bedeaa24a45d704262f2a98c
Erick Fernando Cobo Enriquez <ecoboe249@ulacit.ed.cr>
2017-03-30 00:40:41 -0600

    Hoy aprendi a usar clases con Python. Vamos a ver como me terminar de ir. -Erick Cobo

--- Tareas.py
+++ Tareas.py
@@ -1,31 +1,21 @@
 class TaresServiciosWeb:
-    def __init__(self):
-        self.data = []
-
-    def __init__(self, the_array, the_result_array):
+    # self means that the constructor is taking in the instance of that object.
+    def __init__(self, the_array):
         self.the_array = the_array
-        self.the_result_array = the_result_array
+        self.the_result_array = []
 
     # This method will handle how to calculate the square root of 1000 numbers that go into an array.
     def float_square_roots(self):
-        the_result_array = []
-        the_array = []
-        # I fill the_array with float values
-        for each_value in range(1000):
-            the_array.append(float(each_value))
-        """
-        I iterate through all the values of "the_array" and calculate their respective square root value
-        it then appends it to "the_result_array" and transforms it into a Dictionary.
-        """
-        for each_number in the_array:
-            square_root = each_number ** (1 / 2)
-            print(the_result_array.append({
-                "Number": each_number,
-                "Square Root": square_root
-            }))
+        print('This prints the square root for each number in the array.')
 
-            print('\n')
-        return the_result_array
+        for each_number in self.the_array:
+            try:
+                square_root = each_number ** (1 / 2)
+                self.the_result_array.append(square_root)
+                print('{} : {}'.format(each_number, square_root))
+            except Exception as ex:
+                return 'Something went wrong: ' + str(ex)
+        return 'Process finished with exit code 0'
 
     def integer_square_roots(self):
         the_result_array = []
@@ -33,4 +23,8 @@ class TaresServiciosWeb:
         import random
         for each_number in range(random.randrange(0, 1000)):
             print('')
-        return None
+        return the_result_array
+
+
+array = TaresServiciosWeb([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16])
+print(TaresServiciosWeb.float_square_roots(array))
